8ad8dde257d57af368119f630bec14f46dc5a7d5
Luke Morton <luke@madebymade.co.uk>
2014-08-26 13:12:00 +0100

    Reorder examples in shipping_matrix_spec

--- spec/unit/models/shipping_matrix_spec.rb
+++ spec/unit/models/shipping_matrix_spec.rb
@@ -1,17 +1,17 @@
 describe Spree::ShippingMatrix do
-  it { is_expected.to respond_to(:rules) }
-
   context 'when created' do
     subject { described_class.create(attrs) }
-
-    context 'and no name provided' do
-      let(:attrs) { {} }
-      it { is_expected.to be_invalid }
-    end
 
     context 'and name provided' do
       let(:attrs) { { name: 'UK next day' } }
       it { is_expected.to be_valid }
     end
+
+    context 'and no name provided' do
+      let(:attrs) { {} }
+      it { is_expected.to be_invalid }
+    end
   end
+
+  it { is_expected.to respond_to(:rules) }
 end
