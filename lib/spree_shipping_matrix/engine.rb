39578a8a6bb8ff13d0d1c6ac15e67ddaa99f6df5
Seb Ashton <seb@sebashton.com>
2014-08-29 18:53:28 +0100

    removes extra blank line

--- lib/spree_shipping_matrix/engine.rb
+++ lib/spree_shipping_matrix/engine.rb
@@ -9,7 +9,6 @@ module SpreeShippingMatrix
       g.test_framework :rspec
     end
 
-
     def self.activate
       Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
