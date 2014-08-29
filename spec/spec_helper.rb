95d351c905c03d85bbdeba8855938518f7e6f19f
Luke Morton <luke@madebymade.co.uk>
2014-08-29 10:17:22 +0100

    Remove pointless line of code :sadface:

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,6 @@ require 'ffaker'
 require 'capybara/poltergeist'
 require 'factory_girl'
 
-include(FactoryGirl)
-
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
