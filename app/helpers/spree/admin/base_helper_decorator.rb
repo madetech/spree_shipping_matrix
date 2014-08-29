335c2db242bcf4d35335deee66b41561e7695fcd
Luke Morton <luke@madebymade.co.uk>
2014-08-29 18:03:49 +0100

    Use alias of original since #super errors in some instances

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -1,9 +1,11 @@
 Spree::Admin::BaseHelper.module_eval do
+  alias_method :original_preference_field_for, :preference_field_for
+
   def preference_field_for(form, field, options)
     if options[:type] == :matrix_id
       form.matrix_field(field, preference_field_options(options))
     else
-      super
+      original_preference_field_for(form, field, options)
     end
   end
 end
