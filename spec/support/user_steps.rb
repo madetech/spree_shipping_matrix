23259776c7e3425caf42dc8c07ea84d515682f75
Luke Morton <luke@madebymade.co.uk>
2014-08-29 10:41:58 +0100

    DRY user steps

--- spec/support/user_steps.rb
+++ spec/support/user_steps.rb
@@ -1,19 +1,15 @@
-def given_i_am_logged_into_the_cms
-  @user = FactoryGirl.create(:admin_user)
-  visit spree.admin_path
+def given_i_am_logged_in(login_path = nil)
+  visit login_path || spree.login_path
+  @user ||= FactoryGirl.create(:user)
   fill_in :spree_user_email, with: @user.email
   fill_in :spree_user_password, with: @user.password
   click_button 'Login'
   @user
 end
 
-def given_i_am_logged_in
-  @user = FactoryGirl.create(:user)
-  visit spree.login_path
-  fill_in :spree_user_email, with: @user.email
-  fill_in :spree_user_password, with: @user.password
-  click_button 'Login'
-  @user
+def given_i_am_logged_into_the_cms
+  @user = FactoryGirl.create(:admin_user)
+  given_i_am_logged_in(spree.admin_path)
 end
 
 def given_i_am_logged_in_as_user_with(role)
